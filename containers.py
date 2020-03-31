e548c618bd6068434d18ff05ead383b6b46e8fce
iomanker <cc929c@gmail.com>
2020-03-31 16:56:30 +0800

    Add vqvae layer and bind to decoder

--- containers.py
+++ containers.py
@@ -2,7 +2,158 @@ import tensorflow as tf
 from blocks import *
 from layers import *
 from models import *
+from losses import *
 import numpy as np
+
+class VQVAE_Generator(tf.keras.Model):
+    def __init__(self, config):
+        super(VQVAE_Generator, self).__init__()
+        n_features     = config['nf']
+        n_features_mlp = config['nf_mlp']
+        down_class     = config['n_downs_class']
+        down_content   = config['n_downs_content']
+        n_mlp_blocks   = config['n_mlp_blks']
+        n_res_blocks   = config['n_res_blks']
+        latent_dim     = config['latent_dim']
+        
+        self.E_content = ContentEncoder(downs=down_content,
+                                        n_res=n_res_blocks,
+                                        n_filters=n_features,
+                                        norm='in',activation='relu',pad_type='reflect')
+        # VQVAE for Content Encoder
+        
+        # D - This value is not that important, usually 64 works.
+        #   - This will not change the capacity in the information-bottleneck.
+        self.embedding_dim = 64
+        # K - The higher this value, the higher the capacity in the information bottleneck.
+        self.num_embeddings = 512
+        self.commitment_cost = 0.25
+        self.pre_vqvae = tf.keras.layers.Conv2D(self.embedding_dim, kernel_size=1, strides=1, padding='valid')
+        self.vq = VectorQuantizer(self.embedding_dim, self.num_embeddings, self.commitment_cost)
+        
+        self.E_class = ClassEncoder(downs=down_class,
+                                    latent_dim=latent_dim,
+                                    n_filters=n_features,
+                                    norm='none',activation='relu',pad_type='reflect')
+        
+        # self.E_content.output_filters*2
+        self.mlp = MLP(out_dim=self.embedding_dim*2,
+                       dim=n_features_mlp,
+                       n_blk=n_mlp_blocks, activation='relu')
+        
+        self.Dec = Decoder(ups=down_content,
+                           n_res=n_res_blocks,
+                           n_filters=self.embedding_dim,
+                           out_dim=3,
+                           activation='relu',pad_type='reflect')
+        
+    def decode(self, content, model_code):
+        adain_params = self.mlp(model_code)
+        imgs = self.Dec(content,adain_params)
+        return imgs
+    
+class VQVAE_FUNIT(tf.keras.Model):
+    def __init__(self, config):
+        super(VQVAE_FUNIT, self).__init__()
+        self.gen = VQVAE_Generator(config['gen'])
+        self.dis = Discriminator(config['dis'])
+        self.opt_gen = tf.keras.optimizers.RMSprop(learning_rate=config['lr_gen'])
+        self.opt_dis = tf.keras.optimizers.RMSprop(learning_rate=config['lr_dis'])
+        
+    def gen_train_step(self, x, config):
+        co_data, cl_data = x
+        xa, la = co_data
+        xb, lb = cl_data
+        with tf.GradientTape() as g_tape:
+            xt_g, xr, xa_gan_feat, xb_gan_feat, vqvae_loss = self.gen_produce(co_data, cl_data, config, True)
+            
+            resp_xr_fake, xr_gan_feat = self.dis(xr, la)
+            resp_xt_fake, xt_gan_feat = self.dis(xt_g, lb)
+            
+            # GAN loss
+            l_adv_t = GANloss.gen_loss(resp_xt_fake,lb)
+            l_adv_r = GANloss.gen_loss(resp_xr_fake,la)
+            l_adv = 0.5 * (l_adv_t + l_adv_r)
+            # Reconstruction loss
+            l_x_rec = recon_loss(xr, xa)
+            l_x_rec = tf.reduce_mean(l_x_rec)
+            # Feature Matching loss
+            l_c_rec = featmatch_loss(xr_gan_feat, xa_gan_feat)
+            l_c_rec = tf.reduce_mean(l_c_rec)
+            l_m_rec = featmatch_loss(xt_gan_feat, xb_gan_feat)
+            l_m_rec = tf.reduce_mean(l_m_rec)
+            
+            G_loss = config['gan_w'] * l_adv + \
+                     config['r_w'] * l_x_rec + \
+                     config['fm_w'] * (l_c_rec + l_m_rec) + \
+                     vqvae_loss
+            loss = G_loss * (1.0 / config['batch_size'])
+        gen_grad = g_tape.gradient(loss, self.gen.trainable_variables)
+        self.opt_gen.apply_gradients(zip(gen_grad, self.gen.trainable_variables))
+        return G_loss
+            
+    def gen_produce(self, co_data, cl_data, config, training):
+        xa,la = co_data
+        xb,lb = cl_data
+        class_xa = self.gen.E_content(xa)
+        class_pre_vqresult = self.gen.pre_vqvae(class_xa)
+        class_vq = self.gen.vq(class_pre_vqresult, training)
+        
+        style_xa = self.gen.E_class(xa)
+        style_xb = self.gen.E_class(xb)
+        
+        xt = self.gen.decode(class_vq['quantize'], style_xb) # Translation
+        xr = self.gen.decode(class_vq['quantize'], style_xa) # Reconstruction
+        
+        _, xa_gan_feat = self.dis(xa,la)
+        _, xb_gan_feat = self.dis(xb,lb)
+        return xt, xr, xa_gan_feat, xb_gan_feat, class_vq['loss']
+    
+    def dis_train_step(self, x, config):
+        co_data, cl_data = x
+        xa, la = co_data
+        xb, lb = cl_data
+        with tf.GradientTape() as d_tape:
+            resp_real, real_gen_feat, xt_d, resp_fake, fake_gan_feat =\
+                                                self.dis_produce(co_data,cl_data,config,True)
+            # Discriminator - GAN loss
+            l_real = GANloss.dis_loss(resp_real, lb, 'real')
+            l_fake = GANloss.dis_loss(resp_fake, lb, 'fake')
+            # Discriminator - Gradient Penalty
+            l_reg = gradient_penalty(self.dis, xb, lb)
+
+            D_loss = config['gan_w'] * l_real +\
+                     config['gan_w'] * l_fake +\
+                     10 * l_reg
+            loss = D_loss * (1.0 / config['batch_size'])
+        dis_grad = d_tape.gradient(loss, self.dis.trainable_variables)
+        self.opt_dis.apply_gradients(zip(dis_grad, self.dis.trainable_variables))
+        return D_loss
+    
+    def dis_produce(self, co_data, cl_data, config, training):
+        xa,la = co_data
+        xb,lb = cl_data
+        resp_real, real_gan_feat = self.dis(xb,lb)
+        class_xa = self.gen.E_content(xa)
+        class_pre_vqresult = self.gen.pre_vqvae(class_xa)
+        class_vq = self.gen.vq(class_pre_vqresult, training)
+        
+        style_xb = self.gen.E_class(xb)
+        xt = self.gen.decode(class_vq['quantize'], style_xb)
+        resp_fake, fake_gan_feat = self.dis(xt,lb)
+        return resp_real, real_gan_feat, xt, resp_fake, fake_gan_feat
+    
+    def test_step(self, co_data, cl_data, config):
+        xa, la = co_data
+        xb, lb = cl_data
+        return_items = {}
+        xt, xr, xa_gan_feat, xb_gan_feat = self.gen_produce(co_data,cl_data,config,False)
+        return_items['xa'] = xa.numpy()
+        return_items['xb'] = xb.numpy()
+        return_items['xr'] = xr.numpy()
+        return_items['xt'] = xt.numpy()
+        return_items['display_list'] = ['xa','xr','xt','xb']
+        return return_items
 
 class Generator(tf.keras.Model):
     def __init__(self, config):
--- train.py
+++ train.py
@@ -46,7 +46,7 @@ if __name__ == "__main__":
         strategy = tf.distribute.MirroredStrategy()
     else:
         print("Multigpus OFF.")
-        strategy = tf.distribute.OneDeviceStrategy()
+        strategy = tf.distribute.OneDeviceStrategy(device="/gpu:0")
         
     # Datasets
     datasets = get_datasets(config)
@@ -63,82 +63,24 @@ if __name__ == "__main__":
     test_content_dataset = datasets[2]
     test_class_dataset = datasets[3]
     test_dataset = tf.data.Dataset.zip((test_content_dataset, test_class_dataset))
-    
-    # Networks
-    with strategy.scope():
-        networks = FUNIT(config)
-        test_networks = FUNIT(config)
-        
-        # Split Distributed training example: 
-        # -- https://github.com/tensorflow/examples/blob/master/tensorflow_examples/models/nmt_with_attention/distributed_train.py
-        def gen_train_step(x, config):
-            co_data, cl_data = x
-            xa, la = co_data
-            xb, lb = cl_data
-            with tf.GradientTape() as g_tape:
-                xt_g, xr, xa_gan_feat, xb_gan_feat = networks.gen_update(co_data,cl_data,config)
-                
-                resp_xr_fake, xr_gan_feat = networks.dis(xr, la)
-                resp_xt_fake, xt_gan_feat = networks.dis(xt_g, lb)
-                
-                # Generator - GAN loss
-                l_adv_t = GANloss.gen_loss(resp_xt_fake,lb)
-                l_adv_r = GANloss.gen_loss(resp_xr_fake,la)
-                # - NOTICE
-                l_adv = 0.5 * (l_adv_t + l_adv_r)
-                # Generator - Reconstruction loss
-                l_x_rec = recon_loss(xr, xa)
-                l_x_rec = tf.reduce_mean(l_x_rec)
-                # Generator - Feature Matching loss
-                l_c_rec = featmatch_loss(xr_gan_feat, xa_gan_feat)
-                l_c_rec = tf.reduce_mean(l_c_rec)
-                l_m_rec = featmatch_loss(xt_gan_feat, xb_gan_feat)
-                l_m_rec = tf.reduce_mean(l_m_rec)
-                
-                G_loss = config['gan_w'] * l_adv +\
-                         config['r_w'] * l_x_rec +\
-                         config['fm_w'] * (l_c_rec + l_m_rec)
-                
-                loss = G_loss * (1.0 / config['batch_size'])
-            gen_grad = g_tape.gradient(loss, networks.gen.trainable_variables)
-            networks.opt_gen.apply_gradients(zip(gen_grad, networks.gen.trainable_variables))
-            return G_loss
-        
-        def dis_train_step(x, config):
-            co_data, cl_data = x
-            xa, la = co_data
-            xb, lb = cl_data
-            with tf.GradientTape() as d_tape:
-                resp_real, real_gen_feat, xt_d, resp_fake, fake_gan_feat =\
-                                                    networks.dis_update(co_data,cl_data,config)
-                # Discriminator - GAN loss
-                l_real = GANloss.dis_loss(resp_real, lb, 'real')
-                l_fake = GANloss.dis_loss(resp_fake, lb, 'fake')
-                # Discriminator - Gradient Penalty
-                l_reg = gradient_penalty(networks.dis, xb, lb)
-
-                D_loss = config['gan_w'] * l_real +\
-                         config['gan_w'] * l_fake +\
-                         10 * l_reg
-                loss = D_loss * (1.0 / config['batch_size'])
-            dis_grad = d_tape.gradient(loss, networks.dis.trainable_variables)
-            networks.opt_dis.apply_gradients(zip(dis_grad, networks.dis.trainable_variables))
-            return D_loss
         
     # Tensorboard - Setup
-    import datetime
-    current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-    train_log_dir = os.path.join(opts.log_path, current_time, 'train')
-    train_summary_writer = tf.summary.create_file_writer(train_log_dir)
+    # import datetime
+    # current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    # train_log_dir = os.path.join(opts.log_path, current_time, 'train')
+    # train_summary_writer = tf.summary.create_file_writer(train_log_dir)
         
-    # Start Training
+    # Networks & Start Training
     with strategy.scope():
+        networks = VQVAE_FUNIT(config)
+        test_networks = VQVAE_FUNIT(config)
+        
         @tf.function
         def distributed_train_step(dataset_inputs, config):
-            dis_per_replica_losses = strategy.experimental_run_v2(dis_train_step, args=(dataset_inputs, config))
+            dis_per_replica_losses = strategy.experimental_run_v2(networks.dis_train_step, args=(dataset_inputs, config))
             dis_loss = strategy.reduce(tf.distribute.ReduceOp.MEAN, dis_per_replica_losses, axis=None)
             
-            gen_per_replica_losses = strategy.experimental_run_v2(gen_train_step, args=(dataset_inputs, config))
+            gen_per_replica_losses = strategy.experimental_run_v2(networks.gen_train_step, args=(dataset_inputs, config))
             gen_loss = strategy.reduce(tf.distribute.ReduceOp.MEAN, gen_per_replica_losses, axis=None)
             return gen_loss, dis_loss
         
@@ -165,9 +107,9 @@ if __name__ == "__main__":
                     G_loss, D_loss = distributed_train_step(x, config)
                     print(" (%d/%d) G_loss: %.4f, D_loss: %.4f, time: %.5f" % (iteration,config['max_iter'],G_loss,D_loss,(time.time() - start_time)))
                     # Tensorboard - Record losses
-                    with train_summary_writer.as_default():
-                        tf.summary.scalar('g_loss', G_loss, step=iteration)
-                        tf.summary.scalar('d_loss', D_loss, step=iteration)
+                    # with train_summary_writer.as_default():
+                    #     tf.summary.scalar('g_loss', G_loss, step=iteration)
+                    #     tf.summary.scalar('d_loss', D_loss, step=iteration)
 
                     # Test Step (Print this interval result)
                     if iteration % config['image_save_iter'] == 0 or\
@@ -183,13 +125,13 @@ if __name__ == "__main__":
                         output_train_dataset = train_dataset.batch(GLOBAL_BATCH_SIZE).take(opts.test_batch_size)
                         output_test_dataset = test_dataset.batch(GLOBAL_BATCH_SIZE).take(opts.test_batch_size)
                         for idx,(co_data, cl_data) in output_train_dataset.enumerate():
-                            test_returns = test_step(test_networks,co_data,cl_data,config)
+                            test_returns = test_networks.test_step(co_data,cl_data,config)
                             write_images((test_returns['xa'],test_returns['xr'],test_returns['xt'],test_returns['xb']), 
                                          test_returns['display_list'],
                                          os.path.join(opts.output_path, 'train_%s_%02d' % (key_str, idx)),
                                          max(config['crop_image_height'], config['crop_image_width']))
                         for idx,(co_data, cl_data) in output_test_dataset.enumerate():
-                            test_returns = test_step(test_networks,co_data,cl_data,config)
+                            test_returns = test_networks.test_step(co_data,cl_data,config)
                             write_images((test_returns['xa'],test_returns['xr'],test_returns['xt'],test_returns['xb']), 
                                          test_returns['display_list'],
                                          os.path.join(opts.output_path, 'test_%s_%02d' % (key_str, idx)),
@@ -198,7 +140,8 @@ if __name__ == "__main__":
                     if iteration >= config['max_iter']:
                         print("End of iteration")
                         break
-            except TypeError:
+            except TypeError as err:
+                print(err)
                 print("Distributed Training doesn't have a functionality of drop_remainder,\n  keep training and still waiting for Tensorflow fixing this problem.")
             if iteration >= config['max_iter']:
                 break
